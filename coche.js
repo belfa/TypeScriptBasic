5de6e830389f041b2d43ccf82ab6f81889f30780
belfa <miguel36410@gmail.com>
2017-01-29 20:19:22 +0100

    Primeros Métodos
    Añadimos los métodos Acelerar y Frenar a la clase coche y los
    utilizamos.

--- coche.js
+++ coche.js
@@ -22,6 +22,14 @@ var Coche = (function () {
         this.velocidad = velocidad;
         return this;
     };
+    Coche.prototype.acelerar = function () {
+        this.velocidad++;
+        return this;
+    };
+    Coche.prototype.frenar = function () {
+        this.velocidad--;
+        return this;
+    };
     return Coche;
 }());
 var coche1 = new Coche();
@@ -30,6 +38,6 @@ var coche3 = new Coche();
 coche1.setColor('rojo').setModelo('Ferrari').setVelocidad(120.3);
 coche2.setColor('azul').setModelo('Poscher').setVelocidad(45);
 coche3.setColor('verde').setModelo('Mercedes').setVelocidad(67);
-console.log("Color del coche 1 es:" + coche1.getColor() + " del modelo " + coche1.getModelo() + " y va a una velocidad " + coche1.getVelocidad());
-console.log("Color del coche 2 es:" + coche2.getColor() + " del modelo " + coche2.getModelo() + " y va a una velocidad " + coche2.getVelocidad());
-console.log("Color del coche 3 es:" + coche3.getColor() + " del modelo " + coche3.getModelo() + " y va a una velocidad " + coche3.getVelocidad());
+console.log("Color del coche 1 es:" + coche1.getColor() + " del modelo " + coche1.getModelo() + " y va a una velocidad " + coche1.getVelocidad() + " aceleramos " + coche1.acelerar().getVelocidad());
+console.log("Color del coche 2 es:" + coche2.getColor() + " del modelo " + coche2.getModelo() + " y va a una velocidad " + coche2.getVelocidad() + " frenamos " + coche2.frenar().getVelocidad());
+console.log("Color del coche 3 es:" + coche3.getColor() + " del modelo " + coche3.getModelo() + " y va a una velocidad " + coche3.getVelocidad() + " ambas " + coche3.frenar().acelerar().getVelocidad());
--- coche.ts
+++ coche.ts
@@ -29,6 +29,16 @@ class Coche{
         this.velocidad = velocidad;
         return this;
     }
+
+    public acelerar(){
+        this.velocidad++;
+        return this;
+    }
+
+    public frenar(){
+        this.velocidad--;
+        return this;
+    }
 }
 
 var coche1 = new Coche();
@@ -41,6 +51,6 @@ coche1.setColor('rojo').setModelo('Ferrari').setVelocidad(120.3);
 coche2.setColor('azul').setModelo('Poscher').setVelocidad(45);
 coche3.setColor('verde').setModelo('Mercedes').setVelocidad(67);
 
-console.log("Color del coche 1 es:"+coche1.getColor()+" del modelo "+coche1.getModelo()+" y va a una velocidad "+coche1.getVelocidad());
-console.log("Color del coche 2 es:"+coche2.getColor()+" del modelo "+coche2.getModelo()+" y va a una velocidad "+coche2.getVelocidad());
-console.log("Color del coche 3 es:"+coche3.getColor()+" del modelo "+coche3.getModelo()+" y va a una velocidad "+coche3.getVelocidad());
+console.log("Color del coche 1 es:"+coche1.getColor()+" del modelo "+coche1.getModelo()+" y va a una velocidad "+coche1.getVelocidad()+" aceleramos "+coche1.acelerar().getVelocidad());
+console.log("Color del coche 2 es:"+coche2.getColor()+" del modelo "+coche2.getModelo()+" y va a una velocidad "+coche2.getVelocidad()+" frenamos "+coche2.frenar().getVelocidad());
+console.log("Color del coche 3 es:"+coche3.getColor()+" del modelo "+coche3.getModelo()+" y va a una velocidad "+coche3.getVelocidad()+" ambas "+coche3.frenar().acelerar().getVelocidad());

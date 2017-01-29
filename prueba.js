b36aa9c88484e12016c0dbc40fd14e0b21a83673
belfa <miguel36410@gmail.com>
2017-01-29 19:58:23 +0100

    Pruebas de tipados
    Algunas pruebas de tirados con las variables y con las funciones.

--- prueba.js
+++ prueba.js
@@ -5,3 +5,28 @@ var nombre = 'Miguel Patiño';
 var resultado = holaMundo(nombre);
 var etiqueta = document.getElementById('container');
 etiqueta.innerHTML = resultado;
+// Variables y tipos
+var nombre = "Miguel Patiño";
+var edad = 23;
+var programador = true;
+var langs = ["PHP", "JavaScript", "CSS"];
+var cualquiera = "cualquier tipo";
+var a = 7;
+var b = 12;
+if (a === 7) {
+    var a_1 = 4;
+    var b_1 = 1;
+    console.log("DENTRO DE IF: " + a_1 + " - " + b_1);
+}
+console.log("FUERA DEL IF: " + a + " - " + b);
+etiqueta.innerHTML = nombre + " - " + edad;
+// Funciones y tipado
+function devuelveNumero(num) {
+    return "Numero devuelto" + num;
+}
+function devuelveString(texto) {
+    if (texto == "hola")
+        return 66;
+    else
+        return 90;
+}
--- prueba.ts
+++ prueba.ts
@@ -9,3 +9,36 @@ var resultado = holaMundo(nombre);
 var etiqueta = <HTMLElement>document.getElementById('container');
 etiqueta.innerHTML = resultado;
 
+// Variables y tipos
+var nombre:string = "Miguel Patiño";
+var edad:number = 23;
+var programador:boolean = true;
+var langs:Array<string> = ["PHP","JavaScript","CSS"];
+var cualquiera:any = "cualquier tipo";
+
+
+var a = 7;
+var b = 12;
+
+if(a === 7){
+    let a = 4;
+    let b = 1;
+
+    console.log("DENTRO DE IF: "+a+" - "+b);
+}
+
+console.log("FUERA DEL IF: "+a+" - "+b);
+
+etiqueta.innerHTML = nombre+" - "+edad;
+
+// Funciones y tipado
+function devuelveNumero(num:number):string{
+    return "Numero devuelto"+num;
+}
+
+function devuelveString(texto:string){
+    if(texto == "hola") return 66;
+    else return 90;
+}
+
+
